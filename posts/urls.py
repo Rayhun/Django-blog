9afcb2a234ff2735270d5741bb0cc3bd65b0e877
rayhan <rayhunkhan27@gmail.com>
2020-09-20 01:30:21 +0600

    post 'create update delete'

--- posts/urls.py
+++ posts/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.post_home, name = "post_home"),
     path('create/', views.post_create, name = "post_create"),
-    path('detale/<int:pk>', views.post_detale, name = "post_detale"),
-    path('list/', views.post_list, name = "post_list"),
-    path('update/', views.post_update, name = "post_update"),
-    path('delete/', views.post_delete, name = "post_delete"),
+    path('single/<int:pk>', views.post_detale, name = "post_detail"),
+    path('list/<int:pk>', views.post_list, name = "post_list"),
+    path('update/<int:pk>', views.post_update, name = "post_update"),
+    path('delete/<int:pk>', views.post_delete, name = "post_delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,28 +1,34 @@
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404 , redirect
 
 # Create your views here.
 from .forms import PostForm
 from .models import Post
  
 def post_home(request):
-	queryset = Post.objects.all() #Post Why?
+	queryset = Post.objects.all()
 	context = {
 		"object_list":queryset,
 		"page_title": "Home Page"
 	}
+	return render(request,"base.html", context)
 
-	return render(request,"index.html", context)
 
 def post_create(request):
 	form = PostForm(request.POST or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		messages.success(request, "Success Created")
+		return redirect ('post_home')
+	else:
+		messages.error(request, "Not Success Created")
 	context = {
 		"form": form,
 	}
 	return render(request,"post-form.html", context)
+
 
 def post_detale(request,pk):
 	quryis = get_object_or_404(Post,pk=pk)
@@ -30,22 +36,46 @@ def post_detale(request,pk):
 		"name": quryis.title,
 		"quryis":quryis,
 	}
-	return render(request,"post-create.html", context)
+	return render(request,"single.html", context)
+
 
 def post_list(request):
 	context = {
 		"name": "List"
 	}
-	return render(request,"index.html", context)
+	return render(request,"base.html", context)
 
-def post_update(request):
-	context = {
-		"name": "Update"
-	}
-	return render(request,"post-update.html", context)
 
-def post_delete(request):
-	context = {
-		"name": "Delete"
-	}
-	return render(request,"index.html", context)
+def post_update(request,pk):
+	if request.method == 'GET':
+		instence = get_object_or_404(Post,pk=pk)
+		form = PostForm(instance = instence )
+		context = {
+			"object":instence,
+			"form":form,
+		}
+		return render(request,"post-update.html", context)
+	else:	
+		instence = get_object_or_404(Post,pk=pk)
+		form = PostForm(request.POST or None, instance = instence )
+		if form.is_valid():
+			instance = form.save(commit=False)
+			instance.save()
+			messages.success(request, "Update Success")
+			# return redirect ('post_home')
+		else:
+			messages.error(request, "Update Not Success")
+		context = {
+			"object":instence,
+			"form":form,
+		}
+		return render(request,"post-update.html", context)
+
+def post_delete(request,pk):
+	quryis = get_object_or_404(Post,pk=pk)
+	quryis.delete()
+	messages.success(request, "Delete Success")
+	return redirect ('post_home')
+	# context = {
+	# 	"name": "Delete"
+	# }
