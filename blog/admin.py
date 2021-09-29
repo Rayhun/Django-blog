d31eac1b743a6c84cd7205eb27099322be435e9c
Rayhun <rayhunkhan27@gmail.com>
2021-09-29 12:20:23 -0700

    update admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, BlogPost
+from .models import Category, BlogPost, BlogComment
 
 
 class AdminCategory(admin.ModelAdmin):
@@ -10,3 +10,8 @@ admin.site.register(Category, AdminCategory)
 class AdminBlogPost(admin.ModelAdmin):
     list_display = ['author', 'title', 'category', 'total_view']
 admin.site.register(BlogPost, AdminBlogPost)
+
+
+class AdminBlogComment(admin.ModelAdmin):
+    list_display = ['post', 'name', 'email', 'mobile']
+admin.site.register(BlogComment, AdminBlogComment)
