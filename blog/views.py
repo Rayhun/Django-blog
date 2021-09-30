06f20a359da6233daf9a0438ebb2618b766d438a
Rayhun <rayhunkhan27@gmail.com>
2021-09-30 14:03:11 -0700

    update blog comment replay query

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, DetailView
-from .models import BlogPost, BlogComment, ReplayBlogComment
+from .models import BlogPost, BlogComment
 from .forms import ReplayCommentForm, CommentForm
 
 
@@ -28,42 +28,33 @@ class BlogDetails(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['comment'] = BlogComment.objects.filter(post=self.object)
+        context['comment'] = BlogComment.objects.filter(
+            post=self.object, parent__isnull=True
+        )
+        context['comment_count'] = BlogComment.objects.filter(
+            post=self.object
+        ).count()
         context['form'] = CommentForm()
-        context['replay_form'] = ReplayCommentForm()
+        context['replay_blog'] = BlogComment.objects.filter(parent=20)
         self.object.total_view += 1
         self.object.save()
         return context
 
     def post(self, request, pk):
         object = self.model.objects.get(pk=pk)
-        # rep_comment = BlogComment.objects.get(post=object)
+        try:
+            replay_comment = int(request.POST.get('replay_comment'))
+            replay = BlogComment.objects.get(pk=replay_comment)
+        except Exception as e:
+            print(e)
+            replay_comment = None
+            replay = None
+        # comment = BlogComment.objects.get(pk=20)
         form = CommentForm(request.POST, request.FILES)
-        replay_form = ReplayCommentForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save(commit=False)
             user.post = object
+            user.parent = replay
             user.save()
-        if replay_form.is_valid():
-            rep_com = replay_form.save(commit=False)
-            # rep_com.comment = str(rep_comment)
-            # rep_com.save()
-        # object = self.model.objects.get(pk=pk)
-        # name = request.POST.get('name')
-        # email = request.POST.get('email')
-        # mobile = request.POST.get('mobile')
-        # website = request.POST.get('website')
-        # image = request.FILES.get('image')
-        # message = request.POST.get('message')
-        # comment = BlogComment.objects.filter(post__pk=pk)
-        # if name and email and message:
-        #     BlogComment.objects.create(
-        #         name=name, email=email, mobile=mobile, website=website,
-        #         message=message, post=object, image=image
-        #     )
-        #     context = {
-        #         'object': object,
-        #         'comment': comment
-        #     }
-        #     return render(request, self.template_name, context)
+            return redirect('blog_details', pk=pk)
         return render(request, self.template_name)
